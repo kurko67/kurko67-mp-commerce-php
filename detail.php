8e0e767b1dee851d21619de75f6ad892cd6957ef
kurko67 <maxi.alaniz@gmail.com>
2020-10-25 13:01:42 -0300

    ok

--- detail.php
+++ detail.php
@@ -1,6 +1,6 @@
-<?php
-    require __DIR__ .  '/pagar.php';
- ?>
+
+    <?php require_once('pagar.php'); ?>
+
 
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
