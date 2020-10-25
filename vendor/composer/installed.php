5f06daaefb946210fcafccfc26b91a2a6d5d234f
kurko67 <maxi.alaniz@gmail.com>
2020-10-25 11:09:57 -0300

    integracion

---
+++ vendor/composer/installed.php
@@ -0,0 +1,114 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8a3b605818d316213b61ec444397605c9b94b3bd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a3b605818d316213b61ec444397605c9b94b3bd',
+    ),
+    'doctrine/annotations' => 
+    array (
+      'pretty_version' => '1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '88fb6fb1dae011de24dd6b632811c1ff5c2928f5',
+    ),
+    'doctrine/cache' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13e3381b25847283a91948d04640543941309727',
+    ),
+    'doctrine/collections' => 
+    array (
+      'pretty_version' => '1.6.7',
+      'version' => '1.6.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55f8b799269a1a472457bd1a41b4f379d4cfba4a',
+    ),
+    'doctrine/common' => 
+    array (
+      'pretty_version' => '2.13.3',
+      'version' => '2.13.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3812c026e557892c34ef37f6ab808a6b567da7f',
+    ),
+    'doctrine/event-manager' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '41370af6a30faa9dc0368c4a6814d596e81aba7f',
+    ),
+    'doctrine/inflector' => 
+    array (
+      'pretty_version' => '1.3.1',
+      'version' => '1.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ec3a55242203ffa6a4b27c58176da97ff0a7aec1',
+    ),
+    'doctrine/lexer' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1febd6c3ef84253d7c815bed85fc622ad207a9f8',
+    ),
+    'doctrine/persistence' => 
+    array (
+      'pretty_version' => '1.3.8',
+      'version' => '1.3.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7a6eac9fb6f61bba91328f15aa7547f4806ca288',
+    ),
+    'doctrine/reflection' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55e71912dfcd824b2fdd16f2d9afe15684cfce79',
+    ),
+    'mercadopago/dx-php' => 
+    array (
+      'pretty_version' => '2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b17ca2d555512b6489067e3166bb4beaab206cff',
+    ),
+  ),
+);
---
+++ vendor/integracion.php
@@ -0,0 +1,74 @@
+<?php
+
+$title=$_GET['title'];
+$price=$_GET['price'];
+$unit=$_GET['unit'];
+$img=$_GET['img'];
+$desc=$_GET['desc'];
+$external=$_GET['external'];
+
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->id = "1234";
+$item->title = $title;
+$item->description = $desc;
+$item->picture_url = $img;
+$item->quantity = $unit;
+$item->currency_id = "ARS";
+$item->unit_price = $price;
+$preference->items = array($item);
+
+$preference->notification_url = "https://kurko67-mp-commerce-php.herokuapp.com/noti.php?source_news=webhooks";
+
+$preference->payment_methods = array(
+
+    // exluimos amex y atm
+    "excluded_payment_methods" => array(
+    array("id" => "amex" ) ),
+
+    "excluded_payment_types" => array(
+    array("id" => "atm" ) ),
+
+    "installments" => 6
+);
+
+$preference->external_reference = "maxidalaniz@hotmail.com";
+
+$preference->back_urls = array(
+    "success" => "https://kurko67-mp-commerce-php.herokuapp.com/success.php?collection_id=[PAYMENT_ID]&external_reference=[EXTERNAL_REFERENCE]&payment_type=[PAYMENT_METHOD_ID]",
+    "failure" => "https://kurko67-mp-commerce-php.herokuapp.com/failure.php",
+    "pending" => "https://kurko67-mp-commerce-php.herokuapp.com/pending.php"
+);
+
+$preference->auto_return = "approved";
+$preference->save();
+
+  $payer = new MercadoPago\Payer();
+  $payer->name = "Lalo";
+  $payer->surname = "Landa";
+  $payer->email = "test_user_63274575@testuser.com";
+  $payer->date_created = "2020-09-29T23:58:41.425-03:00";
+  $payer->phone = array(
+    "area_code" => "11",
+    "number" => "22223333"
+  );
+
+  $payer->address = array(
+    "street_name" => "False",
+    "street_number" => 123,
+    "zip_code" => "1111"
+    );
+
+?>
